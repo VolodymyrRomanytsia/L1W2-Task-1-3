1d018fc2dd0743815f89d342a70b64ce44929ba8
VolodymyrRomanytsia <romanyca@gmail.com>
2018-08-22 23:07:42 +0300

    Fixed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-
-const button = document.querySelector('.button');
-button.onclick = () => {
+const start = () => {
+	const number = [];
 	const strnum = document.getElementById('arr').value.split(',');
-	let number = [];
 	for (let i = 0; i < strnum.length; i++) {
-		number.push(parseInt(strnum[i]));	
+		number.push(parseInt(strnum[i]));
 	}
-	let sumNum = 0;
-	for (let i = 0; i < number.length; i++) {
-		sumNum += number[i];
+	
+	const average = number => {
+		let sumNum = 0;
+		for (let i = 0; i < number.length; i++) {
+			sumNum += number[i];	
+		}
+		let averNum = sumNum/number.length;
+		return averNum;
 	}
-	let averNum = sumNum/number.length;
-	let result = '[' + averNum +']';
-	document.getElementById("result").innerHTML = result;
+	
+	let averNum = average(number);
+	const result = `[ ${averNum} ]`;
+	document.getElementById("result").innerText = result;
 }
+const button = document.querySelector('.button');
+button.addEventListener('click', start); 
 
 
